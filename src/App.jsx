2d50566164ed13427192838a4a5590f4bdf89091
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-21 23:27:25 +0700

    Refactor App component to include UserContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import { UserContextProvider } from './context/userContext';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Home />
+      <UserContextProvider>
+        <Navbar />
+        <Outlet />
+      </UserContextProvider>
     </div>
   );
 };
