8222beb318d05e72902d1e6aedb944f0459feac5
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-21 16:11:22 +0700

    Add HeroShapeDesktop component and update homepage layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,27 @@
 import Companies from '../components/Companies';
 import Description from '../components/Description';
+import { HeroShapeDesktop } from '../components/ui/Shapes';
 import Button from '../layout/Button';
 
 const Home = () => {
   return (
     <>
-      <div className=" w-full h-screen home bg-no-repeat bg-cover bg-left md:bg-center flex  items-center px-5 lg:px-[72px] ">
-        <div className="max-w-[400px] md:max-w-[450px] lg:max-w-[500px] flex flex-col gap-7">
-          <h1 className="text-[30px] md:text-[40px] lg:text-[50px] font-bold text-green-500">CIPTAKAN LINGKUNGAN BERSIH UNTUK LINGKUNGAN SEHAT BERKELANJUTAN.</h1>
+      <div className="max-w-6xl grid w-full mx-auto md:bg-center grid-cols-2 max-[980px]:grid-cols-1 pt-20 items-center px-5 lg:px-[72px]">
+        <div className="absolute -right-0 -z-10 ">
+          <HeroShapeDesktop />
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-4xl font-bold text-black">
+            CIPTAKAN <span className="bg-[#11BB60] text-white">LINGKUNGAN</span> BERSIH UNTUK LINGKUNGAN <span className="bg-[#11BB60] text-white">SEHAT</span> BERKELANJUTAN.
+          </h1>
 
-          <p className="py-2 text-lg leading-loose">GreenWorldAware adalah website untuk anda yang sadar lingkungan, memberikan informasi, tips, dan wadah untuk membantu anda membuat kontribusi kebersihan demi bumi yang lebih bersih.</p>
+          <p className="py-2 text-lg ">GreenWorldAware adalah website untuk anda yang sadar lingkungan, memberikan informasi, tips, dan wadah untuk membantu anda membuat kontribusi kebersihan demi bumi yang lebih bersih.</p>
           <div>
             <Button type="daftar" title="Sign Up" />
           </div>
+        </div>
+        <div className="flex justify-center max-[980px]:order-first">
+          <img src="https://raw.githubusercontent.com/Skilvul-FS13/Mobile-Responsive-Website/master/img/hero-picture.png" alt="hero-image" width={500} />
         </div>
       </div>
       <Companies />
