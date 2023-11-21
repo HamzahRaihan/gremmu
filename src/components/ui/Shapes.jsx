657c74e6487204de7ff40d59bd2d5b707d53d52d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-21 16:10:51 +0700

    Add HeroShapeDesktop and HeroShapeMobile components

---
+++ src/components/ui/Shapes.jsx
@@ -0,0 +1,15 @@
+export const HeroShapeDesktop = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="887" height="742" viewBox="0 0 887 742" fill="none">
+      <path d="M250.5 451.5C250.501 269.482 -51.2914 207.628 8.5001 0L886.632 -4.90294e-09L886.629 742L492.5 742C400.931 742 250.5 633.518 250.5 451.5Z" fill="#11BB60" />
+    </svg>
+  );
+};
+
+export const HeroShapeMobile = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1513" height="655" viewBox="0 0 1513 655" fill="none">
+      <path d="M0.00365703 429.509C0.00656688 55.9848 0.00364675 433.008 0.00390625 0L1512.2 -8.36845e-09C1512.2 159.219 1512.2 255.479 1512.2 429.509C1052.59 728.883 459.618 728.883 0.00365703 429.509Z" fill="#11BB60" />
+    </svg>
+  );
+};
