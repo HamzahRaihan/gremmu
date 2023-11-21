9eda731acf5854c4c57dbff614774426f38215fc
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-21 10:57:12 +0700

    Add Companies and Description components to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,23 @@
+import Companies from '../components/Companies';
+import Description from '../components/Description';
 import Button from '../layout/Button';
 
 const Home = () => {
   return (
-    <div className=" w-full h-screen home bg-no-repeat bg-cover bg-left md:bg-center flex  items-center px-5 lg:px-[72px] ">
-      <div className="max-w-[400px] md:max-w-[450px] lg:max-w-[500px] flex flex-col gap-7">
-        <h1 className="text-[30px] md:text-[40px] lg:text-[50px] font-bold text-green-500">CIPTAKAN LINGKUNGAN BERSIH UNTUK LINGKUNGAN SEHAT BERKELANJUTAN.</h1>
+    <>
+      <div className=" w-full h-screen home bg-no-repeat bg-cover bg-left md:bg-center flex  items-center px-5 lg:px-[72px] ">
+        <div className="max-w-[400px] md:max-w-[450px] lg:max-w-[500px] flex flex-col gap-7">
+          <h1 className="text-[30px] md:text-[40px] lg:text-[50px] font-bold text-green-500">CIPTAKAN LINGKUNGAN BERSIH UNTUK LINGKUNGAN SEHAT BERKELANJUTAN.</h1>
 
-        <p className="py-2 text-lg leading-loose">GreenWorldAware adalah website untuk anda yang sadar lingkungan, memberikan informasi, tips, dan wadah untuk membantu anda membuat kontribusi kebersihan demi bumi yang lebih bersih.</p>
-        <div>
-          <Button type="daftar" title="Sign Up" />
+          <p className="py-2 text-lg leading-loose">GreenWorldAware adalah website untuk anda yang sadar lingkungan, memberikan informasi, tips, dan wadah untuk membantu anda membuat kontribusi kebersihan demi bumi yang lebih bersih.</p>
+          <div>
+            <Button type="daftar" title="Sign Up" />
+          </div>
         </div>
       </div>
-    </div>
+      <Companies />
+      <Description />
+    </>
   );
 };
 
