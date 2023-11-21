f0a84bb87848b9f4def6ff2aa9e9b4e0d27356fc
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-21 10:57:05 +0700

    Fix Navbar component styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,34 @@
-import { Link } from "react-scroll";
-import Button from "../layout/Button";
+import Button from '../layout/Button';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
       <div>
-        <div className="flex flex-row justify-between p-5 lg:px-32 px-5 from-backgroundColor to-brightColor shadow">
-          <div>
-            <img src="https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/main-logo-small.png?updatedAt=1697183029244" alt="gwa logo" />
+        <div className="flex flex-row justify-between p-5 px-5 bg-white w-full shadow fixed">
+          <div className="logo">
+            <img src="https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/main-logo-small.png?updatedAt=1697183029244" className="max-[980px]:hidden" alt="gwa logo" />
+            <img src="https://raw.githubusercontent.com/Skilvul-FS13/Mobile-Responsive-Website/master/img/logo3.png" className="hidden max-[980px]:block" width={50} alt="logo" />
           </div>
-          <nav className="hidden md:flex flex-row items-center text-slate-800 font-bold gap-8">
-            <Link to="beranda" spy={true} smooth={true} duration={500} className="cursor-pointer">
+          <nav className="flex flex-row items-center text-slate-800 font-bold gap-8 max-[980px]:hidden">
+            <NavLink to="beranda" spy={true} smooth={true} duration={500} className="cursor-pointer">
               Beranda
-            </Link>
-            <Link to="tentang" spy={true} smooth={true} duration={500} className="cursor-pointer">
+            </NavLink>
+            <NavLink to="tentang" spy={true} smooth={true} duration={500} className="cursor-pointer">
               Tentang Kami
-            </Link>
-            <Link to="berita" spy={true} smooth={true} duration={500} className="cursor-pointer">
+            </NavLink>
+            <NavLink to="berita" spy={true} smooth={true} duration={500} className="cursor-pointer">
               Berita
-            </Link>
-            <Link to="petisi" spy={true} smooth={true} duration={500} className="cursor-pointer">
+            </NavLink>
+            <NavLink to="petisi" spy={true} smooth={true} duration={500} className="cursor-pointer">
               Petisi
-            </Link>
-            <Link to="komunitas" spy={true} smooth={true} duration={500} className="cursor-pointer">
+            </NavLink>
+            <NavLink to="komunitas" spy={true} smooth={true} duration={500} className="cursor-pointer">
               Komunitas
-            </Link>
+            </NavLink>
           </nav>
           <div className="flex items-center gap-3">
-            <Button type= "masuk" title="Masuk" />
+            <Button type="masuk" title="Masuk" />
             <Button type="daftar" title="Daftar" />
           </div>
         </div>
