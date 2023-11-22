145d25e457f0838fbad2da33483ec35bf23e65c4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-22 16:28:58 +0700

    Add login and register links to Sidebar component

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,29 +1,33 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Button from './Button';
 
 function Sidebar() {
   return (
     <div className="p-4 fixed right-0 h-screen w-96 max-[400px]:w-72 max-[300px]:w-52 bg-white">
       <nav className="flex flex-col  text-slate-800 font-bold gap-8">
-        <NavLink to="/" spy={true} smooth={true} duration={500} className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
+        <NavLink to="/" className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
           Beranda
         </NavLink>
-        <NavLink to="tentang" spy={true} smooth={true} duration={500} className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
+        <NavLink to="tentang" className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
           Tentang Kami
         </NavLink>
-        <NavLink to="berita" spy={true} smooth={true} duration={500} className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
+        <NavLink to="berita" className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
           Berita
         </NavLink>
-        <NavLink to="petisi" spy={true} smooth={true} duration={500} className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
+        <NavLink to="petisi" className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
           Petisi
         </NavLink>
-        <NavLink to="komunitas" spy={true} smooth={true} duration={500} className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
+        <NavLink to="komunitas" className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
           Komunitas
         </NavLink>
       </nav>
       <div className="flex pt-5 gap-4">
-        <Button type="masuk" title="Masuk" />
-        <Button type="daftar" title="Daftar" />
+        <Link to="/login">
+          <Button type="masuk" title="Masuk" />
+        </Link>
+        <Link to="/register">
+          <Button type="daftar" title="Daftar" />
+        </Link>
       </div>
     </div>
   );
