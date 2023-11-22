31a46ff57804dbb84605423bfc848d32c8ee725f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-22 21:51:06 +0700

    Fix login and registration bugs

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -2,29 +2,38 @@ import axios from 'axios';
 import { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
-import { TOKEN } from '../constants/key';
+import { ACCOUNT_KEY, TOKEN } from '../constants/key';
 import toast from 'react-hot-toast';
+import { jwtDecode } from 'jwt-decode';
 
 export const UserContext = createContext({
   handleLogin: async () => {},
   handleRegister: async () => {},
+  handleLogout: () => {},
   userData: undefined,
+  token: undefined,
   users: [],
   loading: false,
 });
 
 export const UserContextProvider = ({ children }) => {
   const [userData, setUserData] = useState(undefined);
+  console.log('🚀 ~ file: UserContext.jsx:20 ~ UserContextProvider ~ userData:', userData?.id);
+  const [token, setToken] = useState(undefined);
+  console.log('🚀 ~ file: UserContext.jsx:22 ~ UserContextProvider ~ token:', token);
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     const loginToken = JSON.stringify(localStorage.getItem(TOKEN));
+    const user = JSON.parse(localStorage.getItem(ACCOUNT_KEY));
     if (loginToken) {
-      setUserData(JSON.parse(loginToken));
+      setUserData(user);
+      setToken(loginToken);
     } else {
       setUserData(undefined);
+      setToken(undefined);
     }
   }, []);
 
@@ -44,7 +53,7 @@ export const UserContextProvider = ({ children }) => {
 
   const handleLogin = async (email, password) => {
     setLoading(true);
-    if (password.trim('') == '' || email.trim('') == '') {
+    if (password.trim() == '' || email.trim() == '') {
       toast.error('Form wajib terisi semua');
       setLoading(false);
     } else {
@@ -62,10 +71,11 @@ export const UserContextProvider = ({ children }) => {
           }
         )
         .then((response) => {
-          console.log('🚀 ~ file: UserContext.jsx:65 ~ .then ~ response:', response);
           if (response.status === 200) {
             localStorage.setItem(TOKEN, response.data.data);
-
+            const token = JSON.stringify(localStorage.getItem(TOKEN));
+            const decoded = jwtDecode(token);
+            localStorage.setItem(ACCOUNT_KEY, JSON.stringify(decoded));
             navigate('/');
           } else if (response.status === 404) {
             toast.error('email or password is wrong');
@@ -75,31 +85,52 @@ export const UserContextProvider = ({ children }) => {
           }
         })
         .catch((error) => {
-          toast.error(error.message);
+          if (error.response.status === 404) {
+            toast.error('email or password is wrong');
+          } else {
+            toast.error('Internal server error');
+          }
         });
+
       setLoading(false);
     }
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem(TOKEN);
+    localStorage.removeItem(ACCOUNT_KEY);
+    setUserData(undefined);
+    setToken(undefined);
+  };
+
   const handleRegister = async (firstName, lastName, email, password, gender) => {
     setLoading(true);
-    if (password.trim('') == '' || firstName.trim('') == '' || lastName.trim('') == '' || email.trim('') == '') {
+    if (password.trim() === '' || firstName.trim() === '' || lastName.trim() === '' || email.trim() == '') {
       toast.error('Form wajib terisi semua');
       setLoading(false);
     } else {
       await axios
-        .post('https://backend-final-project-eight.vercel.app/users', {
-          firstName,
-          lastName,
-          email,
-          password,
-          gender,
-          role: 'user',
-        })
+        .post(
+          'https://backend-final-project-eight.vercel.app/users',
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+            gender,
+            role: 'user',
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        )
         .then((response) => {
-          if (response.status == 406) {
+          console.log(response);
+          if (response.status === 406) {
             toast.error('Email sudah digunakan');
-          } else if (response.status == 201) {
+          } else if (response.status === 201) {
             toast.success('Berhasil registrasi');
             navigate('/login');
           } else {
@@ -107,8 +138,13 @@ export const UserContextProvider = ({ children }) => {
           }
         })
         .catch((error) => {
-          toast.error(error.message);
+          if (error.response.status === 406) {
+            toast.error('Email sudah digunakan');
+          } else {
+            toast.error(error.message);
+          }
         });
+
       setLoading(false);
     }
   };
@@ -119,7 +155,7 @@ export const UserContextProvider = ({ children }) => {
     }
   }, [navigate, userData]);
 
-  return <UserContext.Provider value={{ loading, users, userData, handleLogin, handleRegister }}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={{ loading, users, userData, token, handleLogin, handleRegister, handleLogout }}>{children}</UserContext.Provider>;
 };
 
 UserContextProvider.propTypes = {
