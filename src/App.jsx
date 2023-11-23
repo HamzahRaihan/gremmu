9de6dc79b686df9e973ebbfd41f1f229dab0761b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-23 11:03:58 +0700

    Fix import casing in UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import { UserContextProvider } from './context/userContext';
+import { UserContextProvider } from './context/UserContext';
 
 const App = () => {
   return (
