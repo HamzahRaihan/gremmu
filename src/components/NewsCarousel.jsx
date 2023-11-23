c999a4208e30b847eb635cc46d9ddb0851720328
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-23 13:45:19 +0700

    Update NewsCarousel component

--- src/components/NewsCarousel.jsx
+++ src/components/NewsCarousel.jsx
@@ -1,42 +1,42 @@
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const data = [
   {
     id: 1,
-    imageUrl: "https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550",
-    description: "Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.",
+    imageUrl: 'https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550',
+    description: 'Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.',
   },
   {
     id: 2,
-    imageUrl: "https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550",
-    description: "Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.",
+    imageUrl: 'https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550',
+    description: 'Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.',
   },
   {
     id: 3,
-    imageUrl: "https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550",
-    description: "Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.",
+    imageUrl: 'https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550',
+    description: 'Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.',
   },
   {
     id: 4,
-    imageUrl: "https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550",
-    description: "Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.",
+    imageUrl: 'https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550',
+    description: 'Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.',
   },
   {
     id: 5,
-    imageUrl: "https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550",
-    description: "Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.",
+    imageUrl: 'https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550',
+    description: 'Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.',
   },
   {
     id: 6,
-    imageUrl: "https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550",
-    description: "Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.",
+    imageUrl: 'https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/news1.jpeg?updatedAt=1700557487550',
+    description: 'Synchronising scrum masters with the possibility to surprise and delight to the surprise and delight scrum masters.',
   },
 ];
 
@@ -47,21 +47,37 @@ const formatDate = () => {
 
 const NewsCarousel = () => {
   return (
-    <div className="flex justify-between px-20 items-center">
-      <div className="w-full p-10">
-        <h2 className="font-bold text-3xl mb-6">Berita Terkini</h2>
-        <div className="flex w-[454px] h-[16px] bg-[#000000] rounded-[50px] mb-6" />
+    <div className="flex justify-between px-10 items-center mb-10">
+      <div className="w-full ">
+        <div className="flex justify-between items-center">
+          <h2 className="font-bold text-3xl mb-6">Berita Terkini</h2>
+          <div className="flex w-96 h-[16px] bg-[#000000] rounded-[50px] mb-6 max-[980px]:hidden" />
+        </div>
 
         <Swiper
           modules={[Navigation, Pagination, Scrollbar, A11y]}
           spaceBetween={50}
-          slidesPerView={3}
+          slidesPerView={1}
+          loop={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
           navigation
           pagination={{ clickable: true }}
           scrollbar={{ draggable: true }}
           onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-          className="mb-10"
+          onSlideChange={() => console.log('slide change')}
         >
           {data.map((item) => (
             <SwiperSlide key={item.id} className="relative">
