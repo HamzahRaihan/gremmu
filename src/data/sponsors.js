ae85ea8a61b40e74527db2203a820a8d4309711c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-23 13:45:22 +0700

    Add sponsor data to sponsors.js

---
+++ src/data/sponsors.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    title: 'rexel',
+    imageUrl: 'https://ik.imagekit.io/irfantonov111/Rexel%20logo.png?updatedAt=1700466528533',
+  },
+  {
+    title: 'greenpeace',
+    imageUrl: 'https://ik.imagekit.io/irfantonov111/Greenpeace%20logo.png?updatedAt=1700466528378',
+  },
+  {
+    title: 'bbc',
+    imageUrl: 'https://ik.imagekit.io/irfantonov111/BBC%20logo.png?updatedAt=1700466528243',
+  },
+  {
+    titile,
+  },
+];
