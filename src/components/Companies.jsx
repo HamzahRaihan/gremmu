068add55ecf1084386770728e1a2307cc2d8810b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-23 13:45:08 +0700

    Refactor Companies component to use Swiper

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,17 +1,26 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 const Companies = () => {
   return (
-    <div className="w-full bg-white py-[50px]">
-      <div className="md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0">
-        <div className="flex justify-center py-8 md:gap-8 ">
-          <div className="grid md:grid-cols-5 grid-cols-2 gap-40">
-            <img src="https://ik.imagekit.io/irfantonov111/Rexel%20logo.png?updatedAt=1700466528533" alt="rexel logo" />
-            <img src="https://ik.imagekit.io/irfantonov111/Greenpeace%20logo.png?updatedAt=1700466528378" alt="greenpeace logo" />
-            <img src="https://ik.imagekit.io/irfantonov111/Unilever%20logo.png?updatedAt=1700466528356" alt="unilever logo" />
-            <img src="https://ik.imagekit.io/irfantonov111/aiden_adn%20logo.png?updatedAt=1700466528354" alt="aiden logo" />
-            <img src="https://ik.imagekit.io/irfantonov111/BBC%20logo.png?updatedAt=1700466528243" alt="bbc logo" />
-          </div>
-        </div>
-      </div>
+    <div className="flex items-center h-full bg-white">
+      <Swiper spaceBetween={50} slidesPerView={4} onSlideChange={() => console.log('slide change')} onSwiper={(swiper) => console.log(swiper)}>
+        <SwiperSlide>
+          <img src="https://ik.imagekit.io/irfantonov111/Rexel%20logo.png?updatedAt=1700466528533" alt="rexel logo" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src="https://ik.imagekit.io/irfantonov111/Greenpeace%20logo.png?updatedAt=1700466528378" alt="greenpeace logo" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://ik.imagekit.io/irfantonov111/BBC%20logo.png?updatedAt=1700466528243" alt="bbc logo" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://ik.imagekit.io/irfantonov111/aiden_adn%20logo.png?updatedAt=1700466528354" alt="aiden logo" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://ik.imagekit.io/irfantonov111/BBC%20logo.png?updatedAt=1700466528243" alt="bbc logo" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
