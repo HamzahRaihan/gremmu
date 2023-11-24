2dae5c2d69f0e7bda010ab4eafb5692c39a33179
alziputra <alziputra12@gmail.com>
2023-11-24 20:14:42 +0700

    modification about us

--- src/pages/AboutUs/Aboutus.jsx
+++ src/pages/AboutUs/Aboutus.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ContactUs } from "../../components/ContactUs";
 import Footer from "../../layout/Footer";
+import Navbar from "../../layout/Navbar";
 
 const AboutUs = () => {
   return (
     <>
+      <Navbar />
       <div className="container p-4 m-auto">
         <div className="p-5 text-center bg-body-tertiary">
           <div className="container py-5 my-5">
@@ -17,47 +19,43 @@ const AboutUs = () => {
         </div>
       </div>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 py-0">
-            <img src="https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/hero-aboutus.png?updatedAt=1697193730049" alt="Image" className="w-100" />
+      <div className="flex flex-col md:flex-row px-20 h-auto">
+        <div className="md:w-1/2">
+          <img src="https://ik.imagekit.io/alzirahmana/Asset%20-%20mobile%20responsive%20web/hero-aboutus.png?updatedAt=1697193730049" alt="Image" className="w-[800px] h-[700px] sm:w-full object-cover mb-4" />
+        </div>
+        <div className="md:w-1/2 text-[15px]">
+          <div className="md:px-6 lg:px-8 xl:px-10">
+            <h2 className="text-3xl lg:text-4xl font-bold mb-4">Visi</h2>
+            <p className="mb-4">
+              Visi kami adalah menciptakan dunia di mana lingkungan alam dapat berkembang dengan seimbang, tanpa terpengaruh oleh pencemaran dan kerusakan yang tidak perlu. Kami ingin melihat generasi masa depan menikmati alam yang indah,
+              udara bersih, air yang sehat, dan lingkungan yang lestari.
+            </p>
           </div>
-          <div className="col-sm-6">
-            <div className="">
-              <h2 className="text-lg py-3">Visi</h2>
-              <p className="fs-5">
-                Visi kami adalah menciptakan dunia di mana lingkungan alam dapat berkembang dengan seimbang, tanpa terpengaruh oleh pencemaran dan kerusakan yang tidak perlu. Kami ingin melihat generasi masa depan menikmati alam yang indah,
-                udara bersih, air yang sehat, dan lingkungan yang lestari.
-              </p>
-            </div>
-            <div className="">
-              <h2 className="text-lg py-3">Misi</h2>
-              <ol className="fs-5">
-                <li>
-                  Pendidikan dan Kesadaran: Kami berupaya meningkatkan kesadaran masyarakat tentang pentingnya menjaga kebersihan lingkungan. Kami menyelenggarakan seminar, lokakarya, dan kampanye pendidikan untuk memberikan pengetahuan dan
-                  keterampilan yang diperlukan untuk bertindak secara bertanggung jawab terhadap lingkungan.
-                </li>
-                <li>
-                  Aksi Lingkungan: Kami tidak hanya berbicara, kami juga bertindak. Kami secara aktif terlibat dalam proyek-proyek pembersihan dan pelestarian lingkungan. Mulai dari membersihkan pantai hingga penanaman pohon, kami bekerja
-                  bersama komunitas lokal untuk menciptakan perubahan nyata.
-                </li>
-                <li>
-                  Inovasi Berkelanjutan: Kami selalu mencari solusi inovatif untuk permasalahan lingkungan. Dengan bekerja sama dengan ahli lingkungan dan teknologi, kami berusaha mengembangkan produk dan layanan yang ramah lingkungan.
-                </li>
-                <li>
-                  Kemitraan dan Kolaborasi: Kami percaya bahwa perubahan besar hanya bisa dicapai melalui kerja sama. Kami bekerja sama dengan organisasi lingkungan, pemerintah, bisnis, dan masyarakat sipil untuk mencapai tujuan bersama.
-                </li>
-                <li>
-                  Keterbukaan dan Akuntabilitas: Kami bertanggung jawab atas tindakan kami dan selalu berusaha untuk tetap terbuka terhadap umpan balik dari masyarakat. Kami ingin memastikan bahwa setiap tindakan kami berkontribusi pada
-                  keberlanjutan dan kebersihan lingkungan.
-                </li>
-              </ol>
-            </div>
+          <div className="md:px-8 lg:px-10 xl:px-12">
+            <h2 className="text-3xl lg:text-4xl font-bold mb-4">Misi</h2>
+            <ol className="list-decimal pl-6 mb-4">
+              <li>
+                Pendidikan dan Kesadaran: Kami berupaya meningkatkan kesadaran masyarakat tentang pentingnya menjaga kebersihan lingkungan. Kami menyelenggarakan seminar, lokakarya, dan kampanye pendidikan untuk memberikan pengetahuan dan
+                keterampilan yang diperlukan untuk bertindak secara bertanggung jawab terhadap lingkungan.
+              </li>
+              <li>
+                Aksi Lingkungan: Kami tidak hanya berbicara, kami juga bertindak. Kami secara aktif terlibat dalam proyek-proyek pembersihan dan pelestarian lingkungan. Mulai dari membersihkan pantai hingga penanaman pohon, kami bekerja
+                bersama komunitas lokal untuk menciptakan perubahan nyata.
+              </li>
+              <li>Inovasi Berkelanjutan: Kami selalu mencari solusi inovatif untuk permasalahan lingkungan. Dengan bekerja sama dengan ahli lingkungan dan teknologi, kami berusaha mengembangkan produk dan layanan yang ramah lingkungan.</li>
+              <li>
+                Kemitraan dan Kolaborasi: Kami percaya bahwa perubahan besar hanya bisa dicapai melalui kerja sama. Kami bekerja sama dengan organisasi lingkungan, pemerintah, bisnis, dan masyarakat sipil untuk mencapai tujuan bersama.
+              </li>
+              <li>
+                Keterbukaan dan Akuntabilitas: Kami bertanggung jawab atas tindakan kami dan selalu berusaha untuk tetap terbuka terhadap umpan balik dari masyarakat. Kami ingin memastikan bahwa setiap tindakan kami berkontribusi pada
+                keberlanjutan dan kebersihan lingkungan.
+              </li>
+            </ol>
           </div>
         </div>
       </div>
 
-      <div className="py-5 text-center mt-5">
+      <div className=" text-center">
         <div className="text-black text-3xl sm:text-4xl font-bold leading-[60px] py-12">
           <h2 className="py-3">Temui Tim</h2>
         </div>
