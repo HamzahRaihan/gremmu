e084e4cff56d16d3e608a8abb8105560839c80d1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-24 16:09:31 +0700

    Add user authentication functionality to Sidebar component

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,7 +1,11 @@
 import { Link, NavLink } from 'react-router-dom';
 import Button from './Button';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
 function Sidebar() {
+  const { userData, handleLogout } = useContext(UserContext);
+
   return (
     <div className="p-4 fixed z-50 right-0 h-screen w-96 max-[400px]:w-72 max-[300px]:w-52 bg-white ">
       <nav className="flex flex-col text-slate-800 font-bold gap-8">
@@ -17,18 +21,24 @@ function Sidebar() {
         <NavLink to="petisi" className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
           Petisi
         </NavLink>
-        <NavLink to="komunitas" className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
+        <NavLink to="/komunitas" className="flex px-2 items-center h-8 rounded transition-all hover:bg-[#11BB60] hover:text-white active:bg-[#328056] cursor-pointer">
           Komunitas
         </NavLink>
       </nav>
-      <div className="flex pt-5 gap-4">
-        <Link to="/login">
-          <Button type="masuk" title="Masuk" />
-        </Link>
-        <Link to="/register">
-          <Button type="daftar" title="Daftar" />
-        </Link>
-      </div>
+      {!userData ? (
+        <div className="flex pt-5 gap-4">
+          <Link to="/login">
+            <Button type="masuk" title="Masuk" />
+          </Link>
+          <Link to="/register">
+            <Button type="daftar" title="Daftar" />
+          </Link>
+        </div>
+      ) : (
+        <div className="mt-10">
+          <Button type="daftar" title="Keluar" onClick={handleLogout} />
+        </div>
+      )}
     </div>
   );
 }
