21c7f7e18ecbf892f0e6a72d45783ca644abeef9
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-24 16:09:38 +0700

    Add user authentication and redirect

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,13 +1,21 @@
 import { Toaster } from 'react-hot-toast';
 import FormRegister from '../../components/FormRegister';
-import { UserContextProvider } from '../../context/UserContext';
+import { UserContext, UserContextProvider } from '../../context/UserContext';
+import { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 
 function Register() {
+  const { userData } = useContext(UserContext);
+
   return (
-    <UserContextProvider>
-      <FormRegister />
-      <Toaster />
-    </UserContextProvider>
+    <div>
+      {!userData && <Navigate to="/" replace={true} />}
+
+      <UserContextProvider>
+        <FormRegister />
+        <Toaster />
+      </UserContextProvider>
+    </div>
   );
 }
 
