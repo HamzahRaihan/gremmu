9c8d06378a73e46119e8d3406aa146b5fa4ed759
alziputra <alziputra12@gmail.com>
2023-11-24 17:51:01 +0700

    modified styling contact us

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -22,28 +22,20 @@ export const ContactUs = () => {
   };
 
   return (
-    <div className="w-[1512px] h-[686px] px-20 py-16 bg-slate-50 flex-col justify-start items-start gap-16 inline-flex">
-      <div className="w-[1280px] h-[558px] px-8 flex-col justify-start items-start gap-5 flex">
-        <div className="self-stretch pb-10 justify-center items-center inline-flex">
-          <div className="text-black text-[40px] font-bold leading-[60px]">Hubungi Kami</div>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className="self-stretch h-[388px] p-8 bg-slate-100 rounded-lg flex-col justify-start items-start gap-6 flex">
-            <div className="self-stretch justify-start items-start gap-6 inline-flex">
-              <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} className="grow shrink basis-0 self-stretch bg-gray-200 rounded-xl shadow justify-start items-start flex" placeholder="Nama depan" />
-              <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} className="grow shrink basis-0 self-stretch bg-gray-200 rounded-xl shadow justify-start items-start flex" placeholder="Nama belakang" />
-            </div>
-            <div className="self-stretch justify-start items-start gap-6 inline-flex">
-              <input type="text" name="subject" value={formData.subject} onChange={handleChange} className="grow shrink basis-0 self-stretch bg-gray-200 rounded-xl shadow justify-start items-start flex" placeholder="Subjek" />
-            </div>
-            <div className="self-stretch h-[116px] flex-col justify-start items-start gap-2 flex">
-              <textarea name="message" value={formData.message} onChange={handleChange} className="w-[1152px] h-[116px] bg-gray-200 rounded-xl shadow justify-start items-start inline-flex" placeholder="Pesan" />
-            </div>
-            <div className="self-stretch justify-start items-start gap-2 inline-flex">
-              <button type="submit" className="w-[124px] h-11 px-[86px] py-[18px] bg-green-500 rounded-xl justify-center items-center gap-2.5 flex">
-                <div className="text-white text-[14px] font-bold leading-none">Kirim</div>
-              </button>
-            </div>
+    <div className="min-h-screen flex items-center justify-center bg-slate-50">
+      <div className="sm:w-full md:w-[600px] lg:w-[800px] bg-white p-8 rounded-lg shadow-md">
+        <div className="text-black text-3xl md:text-4xl lg:text-5xl font-bold mb-6 text-center">Hubungi Kami</div>
+        <form onSubmit={handleSubmit} className="w-full">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} className="w-full p-2 border rounded-md" placeholder="Nama depan" />
+            <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} className="w-full p-2 border rounded-md" placeholder="Nama belakang" />
+          </div>
+          <input type="text" name="subject" value={formData.subject} onChange={handleChange} className="w-full p-2 mt-4 border rounded-md" placeholder="Subjek" />
+          <textarea name="message" value={formData.message} onChange={handleChange} className="w-full p-2 mt-4 border rounded-md" placeholder="Pesan" />
+          <div className="mt-4">
+            <button type="submit" className="w-full bg-green-500 text-white p-2 rounded-md hover:bg-green-600">
+              Kirim
+            </button>
           </div>
         </form>
       </div>
