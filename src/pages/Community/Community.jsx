ab1338f489d7565f9278cd39aba23d341f8e181e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-26 00:09:49 +0700

    Update styling in Community component

--- src/pages/Community/Community.jsx
+++ src/pages/Community/Community.jsx
@@ -3,7 +3,7 @@ import Posts from './Posts';
 
 function Community() {
   return (
-    <div className="flex flex-col max-w-6xl m-auto pt-32 px-2 gap-10 pb-10">
+    <div className="flex flex-col max-w-6xl m-auto pt-32 px-4 gap-6 pb-10">
       <PostInput />
       <Posts />
     </div>
