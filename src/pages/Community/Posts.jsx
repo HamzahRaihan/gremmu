08adb15266a4c4390b46bdad846e51a3bd24938f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-28 00:06:11 +0700

    Add delete functionality to Posts component

--- src/pages/Community/Posts.jsx
+++ src/pages/Community/Posts.jsx
@@ -5,10 +5,13 @@ import { formatDate } from '../../utils/Utils';
 import { PostSkeleton } from '../../utils/Skeleton';
 import { UserContext } from '../../context/UserContext';
 import CommentsModal from './CommentsModal';
+import { BsThreeDots } from 'react-icons/bs';
+import Swal from 'sweetalert2';
+import { Link } from 'react-router-dom';
 
 const Posts = () => {
   const { userData } = useContext(UserContext);
-  const { posts, loading, handleLike, handleDislike, button, setPostId } = useContext(PostContext);
+  const { posts, loading, handleLike, handleDislike, button, setPostId, handleDeletePost } = useContext(PostContext);
   console.log('🚀 ~ file: Posts.jsx:12 ~ Posts ~ posts:', posts);
 
   const [likeCount, setLikeCount] = useState(posts.map((post) => post.Likes?.length || 0));
@@ -25,6 +28,28 @@ const Posts = () => {
 
   const handleRemoveLike = async (id) => {
     await handleDislike(id);
+  };
+
+  const handleClickDelete = (id) => {
+    Swal.fire({
+      title: 'Apakah kamu yakin?',
+      text: 'Kamu tidak bisa mengembalikan ini!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Tidak',
+      confirmButtonText: 'Hapus',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          preConfirm: await handleDeletePost(id),
+          title: 'Berhasil!',
+          text: 'Unggahan berhasil terhapus!.',
+          icon: 'success',
+        });
+      }
+    });
   };
 
   const handleModalComment = (postId) => {
@@ -47,18 +72,34 @@ const Posts = () => {
           posts.map((item, index) => (
             <div className="flex gap-5" key={item.id}>
               <img
-                className="rounded-full w-12 h-fit object-cover"
+                className="rounded-full w-12 h-fit object-cover max-[980px]:hidden"
                 src={!item.User.image ? 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg' : ''}
                 alt="img-profile"
               />
               <div className="flex flex-col gap-3 bg-neutral-200 w-full rounded-lg px-5 py-2">
                 <div className="flex flex-col">
-                  <h1 className="font-bold">
-                    {item.User.firstName} {item.User.lastName}
-                  </h1>
+                  <div className="flex w-full items-center justify-between gap-2">
+                    <div className="flex items-center gap-4">
+                      <img
+                        className="rounded-full w-9 h-fit object-cover min-[980px]:hidden"
+                        src={!item.User.image ? 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg' : ''}
+                        alt="img-profile"
+                      />
+                      <Link to={`/user/${item.userId}`}>
+                        <h1 className="font-bold">
+                          {item.User.firstName} {item.User.lastName}
+                        </h1>
+                      </Link>
+                    </div>
+                    {item.userId == userData?.id && (
+                      <button onClick={() => handleClickDelete(item.id)}>
+                        <BsThreeDots />
+                      </button>
+                    )}
+                  </div>
                   <p className="text-sm font-light">{formatDate(item.createdAt)}</p>
                 </div>
-                <p>{item.post}</p>
+                <p className="text-sm sm:text-sm md:text-base lg:text-lg">{item.post}</p>
                 {item.image && <img src={item.image} alt="post-images" />}
                 <div className="flex gap-4 items-center">
                   <button
