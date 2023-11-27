e8c3cd807c874db00147430e6697391bf9b187ce
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-28 00:06:06 +0700

    Refactor Community component layout

--- src/pages/Community/Community.jsx
+++ src/pages/Community/Community.jsx
@@ -1,11 +1,15 @@
 import PostInput from './PostInput';
 import Posts from './Posts';
+import SidebarCommunity from './SidebarCommunity';
 
 function Community() {
   return (
-    <div className="flex flex-col max-w-6xl m-auto pt-32 px-4 gap-6 pb-10 ">
-      <PostInput />
-      <Posts />
+    <div className="flex">
+      <SidebarCommunity />
+      <div className="flex flex-col max-w-4xl m-auto pt-32 px-4 gap-6 pb-10 ">
+        <PostInput />
+        <Posts />
+      </div>
     </div>
   );
 }
