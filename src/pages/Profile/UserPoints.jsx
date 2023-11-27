5cfe983f0344068d0c87be195a7565f9e861a51f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-28 00:06:35 +0700

    Add UserPoints component with PropTypes

---
+++ src/pages/Profile/UserPoints.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+export const UserPoints = (props) => {
+  const { userById } = props;
+
+  if (userById.points < 100) {
+    return <p>{userById.points} / 100</p>;
+  } else if (userById.points < 200) {
+    return <p>{userById.points} / 200</p>;
+  } else if (userById.points < 300) {
+    return <p>{userById.points} / 300</p>;
+  } else {
+    return <p>Some other content</p>;
+  }
+};
+
+UserPoints.propTypes = {
+  userById: PropTypes.object,
+};
