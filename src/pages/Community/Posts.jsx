f8bedfa1f18dabd52bbd7dec76ca166c900046a8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-28 15:14:57 +0700

    Add UserBadge component to Posts.jsx

--- src/pages/Community/Posts.jsx
+++ src/pages/Community/Posts.jsx
@@ -8,11 +8,11 @@ import CommentsModal from './CommentsModal';
 import { BsThreeDots } from 'react-icons/bs';
 import Swal from 'sweetalert2';
 import { Link } from 'react-router-dom';
+import { UserBadge } from '../Profile/UserBadge';
 
 const Posts = () => {
   const { userData } = useContext(UserContext);
   const { posts, loading, handleLike, handleDislike, button, setPostId, handleDeletePost } = useContext(PostContext);
-  console.log('🚀 ~ file: Posts.jsx:12 ~ Posts ~ posts:', posts);
 
   const [likeCount, setLikeCount] = useState(posts.map((post) => post.Likes?.length || 0));
   const [isOpen, setIsOpen] = useState(false);
@@ -79,7 +79,7 @@ const Posts = () => {
               <div className="flex flex-col gap-3 bg-neutral-200 w-full rounded-lg px-5 py-2">
                 <div className="flex flex-col">
                   <div className="flex w-full items-center justify-between gap-2">
-                    <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-1">
                       <img
                         className="rounded-full w-9 h-fit object-cover min-[980px]:hidden"
                         src={!item.User.image ? 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg' : ''}
@@ -90,6 +90,9 @@ const Posts = () => {
                           {item.User.firstName} {item.User.lastName}
                         </h1>
                       </Link>
+                      <div className="w-5">
+                        <UserBadge userById={item.User} />
+                      </div>
                     </div>
                     {item.userId == userData?.id && (
                       <button onClick={() => handleClickDelete(item.id)}>
