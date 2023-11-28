a5c22cebbf2c25a55ba462e1ea65328e390f10c3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-28 22:14:09 +0700

    Add logout button to ProfileSidebarMobile component

--- src/pages/Profile/ProfileSidebarMobile.jsx
+++ src/pages/Profile/ProfileSidebarMobile.jsx
@@ -1,20 +1,29 @@
 import { useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
 import ProfileStats from './ProfileStats';
+import Button from '../../layout/Button';
+import { useParams } from 'react-router-dom';
 
 const ProfileSidebarMobile = () => {
-  const { userById } = useContext(UserContext);
+  const { userData, userById, handleLogout } = useContext(UserContext);
+  console.log('🚀 ~ file: ProfileSidebarMobile.jsx:8 ~ ProfileSidebarMobile ~ userData:', userData);
+
+  const { userId } = useParams();
 
   return (
-    <div className="p-4 fixed z-50 h-full w-96 max-[400px]:w-72 max-[300px]:w-52 bg-white pt-28">
+    <div className="p-4 fixed z-50 h-full w-96 max-[400px]:w-72 max-[300px]:w-52 bg-neutral-100 pt-28">
       <div className="flex flex-col items-center">
         <img className="rounded-full w-28 " src={`${userById.image ? userById.image : 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'}`} alt="profile-img" />
         <p className="font-bold text-xl">
           {userById.firstName} {userById.lastName}
         </p>
       </div>
-
       <ProfileStats userById={userById} />
+      {userId == userData?.id && (
+        <div className="w-fit m-auto">
+          <Button type="daftar" title="Log out" onClick={handleLogout} />
+        </div>
+      )}
     </div>
   );
 };
