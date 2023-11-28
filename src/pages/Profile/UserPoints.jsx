745eeeaef4cbc2e2256773a43c319990f9b0b544
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-28 22:14:16 +0700

    Add progress bar and level information to UserPoints component

--- src/pages/Profile/UserPoints.jsx
+++ src/pages/Profile/UserPoints.jsx
@@ -1,16 +1,49 @@
+import { Progress } from 'flowbite-react';
 import PropTypes from 'prop-types';
 
 export const UserPoints = (props) => {
   const { userById } = props;
 
   if (userById.points < 100) {
-    return <p>{userById.points} / 100</p>;
+    return (
+      <div className="flex flex-col">
+        <Progress progress={userById.points} color="green" />
+        <div className="flex justify-between">
+          <p>{userById.points}</p>
+          <p>100</p>
+        </div>
+      </div>
+    );
   } else if (userById.points < 200) {
-    return <p>{userById.points} / 200</p>;
+    return (
+      <div className="flex flex-col">
+        <Progress progress={userById.points / 10} color="green" />
+        <div className="flex justify-between">
+          <p>{userById.points}</p>
+          <p>200</p>
+        </div>
+      </div>
+    );
   } else if (userById.points < 300) {
-    return <p>{userById.points} / 300</p>;
+    return (
+      <div className="flex flex-col">
+        <Progress progress={userById.points} color="green" />
+        <div className="flex justify-between">
+          <p>{userById.points}</p>
+          <p>300</p>
+        </div>
+      </div>
+    );
   } else {
-    return <p>Level Max</p>;
+    return (
+      <div className="flex flex-col">
+        <Progress progress={userById.points} color="green" />
+        <div className="flex justify-between">
+          <p>{userById.points}</p>
+          <p>Level max</p>
+        </div>
+      </div>
+    );
   }
 };
 
