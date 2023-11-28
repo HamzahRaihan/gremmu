dc776ed9babce134614e87ebaba453927e6cce98
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-28 22:13:53 +0700

    Commented out SidebarCommunity component

--- src/pages/Community/Community.jsx
+++ src/pages/Community/Community.jsx
@@ -5,7 +5,7 @@ import SidebarCommunity from './SidebarCommunity';
 function Community() {
   return (
     <div className="flex">
-      <SidebarCommunity />
+      {/* <SidebarCommunity /> */}
       <div className="flex flex-col max-w-4xl m-auto pt-32 px-4 gap-6 pb-10 ">
         <PostInput />
         <Posts />
