438e4ea6f7949d2a80f09deae94b87b675613591
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-29 10:53:42 +0700

    Add Petition component

---
+++ src/pages/Petition/Petition.jsx
@@ -0,0 +1,11 @@
+import ListPetitions from './ListPetitions';
+
+const Petition = () => {
+  return (
+    <div className="max-w-5xl m-auto pt-32 h-screen px-3">
+      <ListPetitions />
+    </div>
+  );
+};
+
+export default Petition;
