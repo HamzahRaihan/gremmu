203cd0b8492b5fa79005225e1433d0923ecdd545
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-29 19:52:35 +0700

    Add PetitionContextProvider and Toaster component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,26 @@ import Navbar from './layout/Navbar';
 import { UserContextProvider } from './context/UserContext';
 import Footer from './layout/Footer';
 import { PostContextProvider } from './context/PostContext';
+import { PetitionContextProvider } from './context/PetitionContext';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div>
-      <UserContextProvider>
-        <PostContextProvider>
-          <Navbar />
-          <Outlet />
-          <Footer />
-        </PostContextProvider>
-      </UserContextProvider>
+    <div className="flex flex-col">
+      <div className="flex-1">
+        <UserContextProvider>
+          <PostContextProvider>
+            <PetitionContextProvider>
+              <Navbar />
+              <Outlet />
+              <div className="flex-shrink-0">
+                <Footer />
+              </div>
+              <Toaster />
+            </PetitionContextProvider>
+          </PostContextProvider>
+        </UserContextProvider>
+      </div>
     </div>
   );
 };
