0ee305c4b19958a38ed89423663fda51b4b2ac5b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-29 19:53:23 +0700

    Update Petition component styling

--- src/pages/Petition/Petition.jsx
+++ src/pages/Petition/Petition.jsx
@@ -2,7 +2,7 @@ import ListPetitions from './ListPetitions';
 
 const Petition = () => {
   return (
-    <div className="max-w-5xl m-auto pt-32 h-screen px-3">
+    <div className="max-w-5xl m-auto pt-32 px-3 pb-5">
       <ListPetitions />
     </div>
   );
