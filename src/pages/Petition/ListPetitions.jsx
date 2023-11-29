d280c0d432a1a1e818c45a6c00321d6bc534fb52
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-29 19:53:20 +0700

    Refactor ListPetitions component

--- src/pages/Petition/ListPetitions.jsx
+++ src/pages/Petition/ListPetitions.jsx
@@ -1,12 +1,27 @@
+import { useContext } from 'react';
+import { PetitionContext } from '../../context/PetitionContext';
+import { PetitionSkeleton } from '../../layout/Skeleton';
+import { Link } from 'react-router-dom';
+
 const ListPetitions = () => {
+  const { petitions, loading } = useContext(PetitionContext);
+  console.log('🚀 ~ file: ListPetitions.jsx:6 ~ ListPetitions ~ petitions:', petitions);
   return (
-    <a href="#" className="flex flex-col w-full items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:w-full md:border hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <img className="object-cover rounded-t-lg md:h-48 md:w-48 md:rounded-none md:rounded-s-lg" src="https://source.unsplash.com/random/800×900/?computer" alt="image" />
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-      </div>
-    </a>
+    <div className="grid grid-cols-1 gap-4">
+      {loading ? (
+        <PetitionSkeleton />
+      ) : (
+        petitions.map((item) => (
+          <Link to={`/petisi/${item.id}`} className="flex flex-col bg-white border border-gray-200 rounded-lg shadow w-full lg:h-40 md:h-full  md:flex-row md:w-full md:border hover:bg-gray-100 " key={item.id}>
+            <img className="object-cover rounded-t-lg h-full md:w-48 md:rounded-none md:rounded-s-lg" src={`${item.image ? item.image : 'https://source.unsplash.com/random/600×700/?computer'}`} alt="image" />
+            <div className="flex flex-col p-4 leading-normal">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-3">{item.description}</p>
+            </div>
+          </Link>
+        ))
+      )}
+    </div>
   );
 };
 
