1133dd3c86ad71b0215da1c0e24869c1b4b1f847
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-29 10:53:35 +0700

    Remove SidebarCommunity component from Community page

--- src/pages/Community/Community.jsx
+++ src/pages/Community/Community.jsx
@@ -1,11 +1,9 @@
 import PostInput from './PostInput';
 import Posts from './Posts';
-import SidebarCommunity from './SidebarCommunity';
 
 function Community() {
   return (
     <div className="flex">
-      {/* <SidebarCommunity /> */}
       <div className="flex flex-col max-w-4xl m-auto pt-32 px-4 gap-6 pb-10 ">
         <PostInput />
         <Posts />
