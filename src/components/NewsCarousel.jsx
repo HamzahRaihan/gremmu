a9c5efabfab6a4739a16e29814e821ce9a149756
alziputra <alziputra12@gmail.com>
2023-12-09 16:49:10 +0700

    update news carousel and sync by client-admin

--- src/components/NewsCarousel.jsx
+++ src/components/NewsCarousel.jsx
@@ -1,22 +1,40 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { useEffect, useState } from "react";
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { recentnews } from '../data/recentnews';
-
-const formatDate = () => {
-  const currentDate = new Date();
+const formatDate = (dateString) => {
+  const currentDate = new Date(dateString);
   return currentDate.toDateString();
 };
 
 const NewsCarousel = () => {
+  const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
+    const fetchNewsData = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/news`);
+        if (response.ok) {
+          const data = await response.json();
+          setNewsData(data.data);
+        } else {
+          console.error("Failed to fetch news data");
+        }
+      } catch (error) {
+        console.error("Error while fetching news data:", error);
+      }
+    };
+
+    fetchNewsData();
+  }, []);
+
   return (
-    <div className="max-w-5xl px-5 m-auto flex justify-between items-center mb-10 ">
-      <div className="w-full ">
+    <div className="max-w-5xl px-5 m-auto flex justify-between items-center mb-10">
+      <div className="w-full">
         <div className="flex justify-between items-center">
           <h2 className="font-bold text-3xl mb-6">Berita Terkini</h2>
           <div className="flex w-96 h-[16px] bg-[#000000] rounded-[50px] mb-6 max-[980px]:hidden" />
@@ -43,14 +61,14 @@ const NewsCarousel = () => {
           }}
           pagination={{ clickable: true }}
         >
-          {recentnews.map((item) => (
+          {newsData.map((item) => (
             <SwiperSlide key={item.id} className="relative">
               <div className="flex items-end h-64">
-                <img src={item.imageUrl} alt={formatDate()} className="object-cover w-full h-full rounded" />
+                <img src={item.image} alt={formatDate(item.createdAt)} className="object-cover w-full h-full rounded" />
               </div>
-              <div className="p-4 text-black mb-10">
-                <h2 className="text-lg font-bold">{formatDate()}</h2>
-                <p className="text-sm mb-4">{item.description}</p>
+              <div className="py-4 text-black mb-10">
+                <h2 className="text-lg font-bold">{formatDate(item.createdAt)}</h2>
+                <p className="text-sm mb-4">{item.description.slice(0, 100)}</p>
                 <button className="font-bold py-2 rounded">Baca Selengkapnya</button>
               </div>
             </SwiperSlide>
