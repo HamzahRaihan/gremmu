b9b17fa40b1135d2f127603c5ee9288ff93ec9b5
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-12-13 19:51:54 +0700

    show news data to user form api

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,7 +1,13 @@
 import { Link } from 'react-router-dom';
 import SearchBar from '../../components/SearchBar';
+import { useContext } from 'react';
+import { NewsContext } from '../../context/NewsContext';
+import { Spinner } from 'flowbite-react';
+import { formatDate } from '../../utils/Utils';
 
 const News = () => {
+  const { news, loading } = useContext(NewsContext);
+  console.log('🚀 ~ file: News.jsx:10 ~ News ~ news:', news);
   return (
     <div className="w-full bg-white py-[50px] mt-24">
       <div className="md:max-w-[1480px] m-auto max-w-[600px] px-4 md:px-0">
@@ -18,7 +24,7 @@ const News = () => {
           <div>
             <p className="py-2 text-lg text-gray-600">Kamis, 27 September 2023</p>
             <h1 className="py-2  text-3xl font-semibold">
-              <Link to="/berita/1">
+              <Link to="/berita/36">
                 <p>Kepedulian dalam Membuang dan Mengelola Sampah: Tanggung Jawab Bersama Masyarakat dan Pemerintah</p>
               </Link>
             </h1>
@@ -31,55 +37,11 @@ const News = () => {
         <section className="bg-gray-2 pb-2 pt-10 px-4 md:px-20 dark:bg-dark lg:pb-5 lg:pt-[120px]">
           <div className="container">
             <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-              <SingleCard
-                date=" Kamis, 27 September 2023"
-                image="https://ik.imagekit.io/irfantonov111/News%20Card.png?updatedAt=1701282294774"
-                CardTitle="Kepedulian dalam Membuang dan Mengelola Sampah: Tanggung Jawab Bersama Masyarakat dan Pemerintah"
-                titleHref="/berita/1"
-                alt="News Image"
-              />
-              <SingleCard
-                date=" Kamis, 27 September 2023"
-                image="https://ik.imagekit.io/irfantonov111/News%20Card.png?updatedAt=1701282294774"
-                CardTitle="Kepedulian dalam Membuang dan Mengelola Sampah: Tanggung Jawab Bersama Masyarakat dan Pemerintah"
-                titleHref="/#"
-                alt="News Image"
-              />
-              <SingleCard
-                date=" Kamis, 27 September 2023"
-                image="https://ik.imagekit.io/irfantonov111/News%20Card.png?updatedAt=1701282294774"
-                CardTitle="Kepedulian dalam Membuang dan Mengelola Sampah: Tanggung Jawab Bersama Masyarakat dan Pemerintah"
-                titleHref="/#"
-                alt="News Image"
-              />
-            </div>
-          </div>
-        </section>
-
-        <section className="bg-gray-2 pb-2 pt-2 px-4 md:px-20 dark:bg-dark lg:pb-10 lg:pt-[5px]">
-          <div className="container">
-            <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-              <SingleCard
-                date=" Kamis, 27 September 2023"
-                image="https://ik.imagekit.io/irfantonov111/News%20Card.png?updatedAt=1701282294774"
-                CardTitle="Kepedulian dalam Membuang dan Mengelola Sampah: Tanggung Jawab Bersama Masyarakat dan Pemerintah"
-                titleHref="/news/1"
-                alt="News Image"
-              />
-              <SingleCard
-                date=" Kamis, 27 September 2023"
-                image="https://ik.imagekit.io/irfantonov111/News%20Card.png?updatedAt=1701282294774"
-                CardTitle="Kepedulian dalam Membuang dan Mengelola Sampah: Tanggung Jawab Bersama Masyarakat dan Pemerintah"
-                titleHref="/#"
-                alt="News Image"
-              />
-              <SingleCard
-                date=" Kamis, 27 September 2023"
-                image="https://ik.imagekit.io/irfantonov111/News%20Card.png?updatedAt=1701282294774"
-                CardTitle="Kepedulian dalam Membuang dan Mengelola Sampah: Tanggung Jawab Bersama Masyarakat dan Pemerintah"
-                titleHref="/#"
-                alt="News Image"
-              />
+              {loading ? (
+                <Spinner />
+              ) : (
+                news.map((item) => <SingleCard date={formatDate(item.updatedAt)} image={item.image} CardTitle={item.title} titleHref={`/berita/${item.id}`} CardDescription={item.description} alt="News Image" key={item.id} />)
+              )}
             </div>
           </div>
         </section>
@@ -94,16 +56,16 @@ const SingleCard = ({ alt, image, date, CardDescription, CardTitle, titleHref })
   return (
     <>
       {/*  */}
-      <div className="mb-2 overflow-hidden rounded-lg bg-white shadow-1 duration-300 hover:shadow-3 dark:bg-dark-2 dark:shadow-card dark:hover:shadow-3">
-        <img src={image} alt={alt} className="w-full" />
-        <div className="p-8 sm:p-9 md:p-7 xl:p-9">
+      <div className="overflow-hidden bg-white shadow-1 duration-300 hover:shadow-3 dark:bg-dark-2 dark:shadow-card dark:hover:shadow-3">
+        <img src={image} alt={alt} className="w-full rounded object-cover h-52" />
+        <div className="p-3">
           <p className="mb-2 text-base leading-relaxed text-body-color dark:text-dark-6">{date}</p>
           <h3>
-            <a href={titleHref ? titleHref : '/#'} className="mb-2 block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
+            <Link to={titleHref} className="mb-2 block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
               {CardTitle}
-            </a>
+            </Link>
           </h3>
-          <p className="mb-2 text-base leading-relaxed text-body-color dark:text-dark-6">{CardDescription}</p>
+          <p className="mb-2 text-base leading-relaxed text-body-color dark:text-dark-6 line-clamp-3">{CardDescription}</p>
         </div>
       </div>
       {/*  */}
