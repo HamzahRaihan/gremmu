9fa45f1896f7ffb0b4f66019ef1471b8226b368c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-12-28 15:09:34 +0700

    Add DonationList component to Donation page

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,5 +1,20 @@
+import DonationList from './DonationList';
+
 const Donation = () => {
-  return <div>Donation</div>;
+  return (
+    <>
+      <div className="max-w-6xl pt-24 m-auto flex-row gap-4 pb-10">
+        <p className="text-center font-bold text-2xl">Donasi</p>
+        <p>
+          Gremmu berjuang untuk masa depan yang lebih baik, dan kami tidak dapat mewujudkannya tanpa Anda. Agar tetap mandiri dan melakukan pekerjaan mendesak yang kita lakukan, Gremmu tidak pernah mengambil uang dari perusahaan atau
+          pemerintah. Kami sepenuhnya mengandalkan dukungan dari individu-individu yang cukup berani untuk melakukan perubahan.
+        </p>
+      </div>
+      <div className="max-w-6xl m-auto">
+        <DonationList />
+      </div>
+    </>
+  );
 };
 
 export default Donation;
