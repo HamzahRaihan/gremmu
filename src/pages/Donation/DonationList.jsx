63c4f26a3664a4a1c66b9e1440e6dac36891c3a5
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-01-01 21:07:32 +0700

    Add user data to donation form

--- src/pages/Donation/DonationList.jsx
+++ src/pages/Donation/DonationList.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { donations } from '../../data/donations';
+import { UserContext } from '../../context/UserContext';
+import { DonationContext } from '../../context/DonationContext';
 
 const DonationList = () => {
-  const [donate, setDonate] = useState({ donation_amount: '' });
+  const { userData } = useContext(UserContext);
+  const { handleDonation } = useContext(DonationContext);
+
+  const [donate, setDonate] = useState({
+    donation_amount: '',
+    firstName: userData?.firstName,
+    lastName: userData?.lastName,
+    email: userData?.email,
+  });
+
   const handleAmountClick = (amount) => {
     setDonate({ ...donate, donation_amount: amount.toString() });
   };
   const handleSubmitDonation = (e) => {
     e.preventDefault();
+    handleDonation(donate);
     alert('submitted');
   };
   console.log(donate);
@@ -20,9 +32,10 @@ const DonationList = () => {
         <form onSubmit={handleSubmitDonation}>
           {donations.map((amount) => (
             <div key={amount}>
-              <button onClick={() => handleAmountClick(amount)}>Rp. {amount.toLocaleString()}</button>{' '}
+              <div onClick={() => handleAmountClick(amount)}>Rp. {amount.toLocaleString()}</div>
             </div>
           ))}
+          <button>Pay</button>
         </form>
         <input type="text" />
       </div>
