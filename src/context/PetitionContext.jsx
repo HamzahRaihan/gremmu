3afa3201e9ef03725aa49fdba727d9729737801d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-01-18 19:41:24 +0700

    Refactor PetitionContext.jsx

--- src/context/PetitionContext.jsx
+++ src/context/PetitionContext.jsx
@@ -1,14 +1,15 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import { UserContext } from "./UserContext";
-import { TOKEN } from "../constants/Key";
-import toast from "react-hot-toast";
+import { createContext, useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { UserContext } from './UserContext';
+import { TOKEN } from '../constants/Key';
+import toast from 'react-hot-toast';
 
 export const PetitionContext = createContext({
   petitions: [],
   detailPetition: [],
+  participate: [],
   loading: false,
   loadingButton: false,
   handleSignature: async () => {},
@@ -17,6 +18,8 @@ export const PetitionContext = createContext({
 export const PetitionContextProvider = ({ children }) => {
   const [petitions, setPetitions] = useState([]);
   const [detailPetition, setDetailPetition] = useState([]);
+  const [participate, setParticipate] = useState([]);
+  console.log('🚀 ~ PetitionContextProvider ~ participate:', participate);
   const [loading, setLoading] = useState(false);
   const [loadingButton, setLoadingButton] = useState(false);
 
@@ -37,7 +40,7 @@ export const PetitionContextProvider = ({ children }) => {
         setLoading(false);
       })
       .catch((error) => {
-        console.error("Internal server error", error.message);
+        console.error('Internal server error', error.message);
       });
   };
   useEffect(() => {
@@ -47,23 +50,46 @@ export const PetitionContextProvider = ({ children }) => {
   const getPetitionByID = async () => {
     setLoading(true);
     await axios
-      .get(`${import.meta.env.VITE_BASE_URL}/petitions/${id ? id : 1}`)
+      .get(`${import.meta.env.VITE_BASE_URL}/petitions/${id}`)
       .then((response) => {
         const petitionDetailData = response.data.data;
         setDetailPetition(petitionDetailData);
         setLoading(false);
       })
       .catch((error) => {
-        console.error("Internal server error", error.message);
+        console.error('Internal server error', error.message);
       });
   };
+
   useEffect(() => {
     getPetitionByID();
   }, [id]);
 
+  const getParticipation = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/signatures/${id}`);
+      const result = response.data.data;
+      setParticipate(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getParticipation();
+  }, [id]);
+
   // add signature
   const handleSignature = async (petitionId) => {
+    const findId = participate.find((user) => user.userId == userData.id);
+
+    if (findId) {
+      toast.success('sudah berpartisipasi');
+      return;
+    }
+
     setLoadingButton(true);
+
     await axios
       .post(
         `${import.meta.env.VITE_BASE_URL}/signatures`,
@@ -73,23 +99,24 @@ export const PetitionContextProvider = ({ children }) => {
         },
         {
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
           },
         }
       )
       .catch((error) => {
         if (error.response.status === 403) {
-          toast.error("Kamu belum login");
+          toast.error('Kamu belum login');
         } else {
           toast.error(error.message);
         }
       });
     await getPetitionByID();
+    await getParticipation();
     setLoadingButton(false);
   };
 
-  return <PetitionContext.Provider value={{ petitions, detailPetition, loading, loadingButton, handleSignature }}>{children}</PetitionContext.Provider>;
+  return <PetitionContext.Provider value={{ petitions, detailPetition, participate, loading, loadingButton, handleSignature }}>{children}</PetitionContext.Provider>;
 };
 
 PetitionContextProvider.propTypes = {
